d9eb068c2f09920aa86b686ba1f6ac4893626aba
Mooni <88727410+moon0727@users.noreply.github.com>
2025-01-19 10:51:34 +0900

    feat: kakao login setting
    - react-native-dotenv 3.4.11 설치
    - axios 1.7.9 설치
    - @react-native-seoul/kakao-login 5.4.1 설치
    - env 추가
    - Bridging Header 추가
    - AppDelegate.mm 카카오 로그인 설정
    - info.plist 수정
    - OnboardingScreen에서 frontend가 kakaologin 주관

---
+++ ios/SwiftBridge.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftBridge.swift
+//  h_APP_y
+//
+//  Created by 김문희 on 1/18/25.
+//
+
+import Foundation
--- src/screens/#1_Login/OnboardingScreen.js
+++ src/screens/#1_Login/OnboardingScreen.js
@@ -4,15 +4,70 @@ import colorLogoEn from '../../assets/logos/colorLogoEn.png';
 import kakaoStart from '../../assets/images/kakaoStart.png';
 
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { API_BASE_URL, KAKAO_CLIENT_ID, KAKAO_REDIRECT_URL } from '@env';
+//import * as KakaoLogins from '@react-native-seoul/kakao-login';
+import KakaoLogins, {
+  login,
+  getProfile,
+} from '@react-native-seoul/kakao-login';
+import { useState } from 'react';
 
 function OnboardingScreen() {
   const navigation = useNavigation();
+  const [result, setResult] = useState();
 
-  const moveKakaoLogin = () => {
-    //kakaoLogin 수행 후 SetProfileScreen으로 이동
-
-    navigation.navigate('SetProfileScreen');
+  const signInWithKakao = async () => {
+    try {
+      const token = await login();
+      //console.log(token.accessToken);
+      setResult(JSON.stringify(token.accessToken));
+      console.log(result);
+      const data = getKakaoProfile();
+      console.log(data);
+    } catch (e) {
+      console.log('login error: ', e);
+    }
+    // navigation.navigate('SetProfileScreen');
   };
+
+  const getKakaoProfile = async () => {
+    return await getProfile()
+      .then(result => {
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+
+  const getUserInfo = async accessToken => {
+    try {
+      const response = await fetch('https://kapi.kakao.com/v2/user/me', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('사용자 정보:', data);
+        return data;
+      } else {
+        console.error('사용자 정보 요청 실패:', response.status);
+      }
+    } catch (error) {
+      console.error('에러 발생:', error);
+    }
+  };
+
+  // const getProgile = async () => {
+  //   try {
+  //     const profile = await getKakaoProgile();
+
+  //   }
+  // }
 
   return (
     <View style={styles.container}>
@@ -21,7 +76,7 @@ function OnboardingScreen() {
         <Image source={colorLogoEn} />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={moveKakaoLogin}>
+        <TouchableOpacity onPress={signInWithKakao}>
           <Image source={kakaoStart} />
         </TouchableOpacity>
       </View>
